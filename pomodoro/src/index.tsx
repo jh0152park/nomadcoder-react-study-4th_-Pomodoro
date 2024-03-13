f508f608bc001448ce2ec7ca38fbf96b47d1b986
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-03-13 16:17:50 +0900

    default setup

--- pomodoro/src/index.tsx
+++ pomodoro/src/index.tsx
@@ -1,7 +1,31 @@
+import { reset } from "styled-reset";
+import { RecoilRoot } from "recoil";
+import { createGlobalStyle } from "styled-components";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  
+  body {    
+    background-color: tomato;
+    color: whitesmoke;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+root.render(
+    <>
+        <RecoilRoot>
+            <GlobalStyle />
+            <ChakraProvider>
+                <App />
+            </ChakraProvider>
+        </RecoilRoot>
+    </>
+);
